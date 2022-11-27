bbb18fc2443fb81e267f1e6fd872dc47ba2abe28
Alex <alexiv.04@mail.ru>
2022-11-27 21:48:45 +0700

    commit 3

--- app/src/main/java/com/example/ivanov_pr23/MainActivity2.java
+++ app/src/main/java/com/example/ivanov_pr23/MainActivity2.java
@@ -22,38 +22,41 @@ public class MainActivity2 extends AppCompatActivity {
     }
 
     public void onClick(View view){
-        SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
-        db.execSQL("CREATE TABLE IF NOT EXISTS users (name TEXT, age INTEGER, UNIQUE(name))");
-        db.execSQL("INSERT OR IGNORE INTO users VALUES ('dhdh', 22), ('Пdhdhdh', 31), ('рпвпвпрв ', 28);");
 
-
-        Cursor query = db.rawQuery("SELECT * FROM users;", null);
-        TextView textView = findViewById(R.id.textView);
         buttonlist = findViewById(R.id.button);
-        textView.setText("");
 
 
-        /*
+
+
         buttonlist.setOnClickListener(new View.OnClickListener(){
             public void onClick(View view){
+                SQLiteDatabase db = getBaseContext().openOrCreateDatabase("app.db", MODE_PRIVATE, null);
+                db.execSQL("CREATE TABLE IF NOT EXISTS users (name TEXT, age INTEGER, UNIQUE(name))");
+                db.execSQL("INSERT OR IGNORE INTO users VALUES ('dhdh', 22), ('Пdhdhdh', 31), ('рпвпвпрв ', 28);");
+                Cursor query = db.rawQuery("SELECT * FROM users;", null);
+                TextView textView = findViewById(R.id.textView);
+
+                textView.setText("");
+
                 while(query.moveToNext()){
                     String name = query.getString(0);
                     int age = query.getInt(1);
                     textView.append("Name: " + name + " Age: " + age + "\n");
                 }
+                query.close();
+                db.close();
             }
-        });*/
+        });
 
 
-
-
+        /*
         while(query.moveToNext()){
             String name = query.getString(0);
             int age = query.getInt(1);
             textView.append("Name: " + name + " Age: " + age + "\n");
         }
         query.close();
-        db.close();
+        db.close();*/
     }
     float x1, x2, y1, y2;
     public boolean onTouchEvent(MotionEvent touchevent){
